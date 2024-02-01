3cbbc29a0d197a305748abc158012980067851e1
adrianariton <Adrian.ariton0@gmail.com>
2024-02-01 15:21:14 +0200

    re2

--- 1_mini-libc copy/src/include/internal/essentials.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-
-#ifndef __ESSENTIALS_H__
-#define __ESSENTIALS_H__	1
-
-#define MIN(a, b)	((a) > (b) ? (b) : (a))
-#define MAX(a, b)	((a) > (b) ? (a) : (b))
-
-#endif
--- 1_mini-libc copy/src/process/sleep.c
+++
@@ -1,8 +0,0 @@
-#include <internal/syscall.h>
-#include <stdlib.h>
-#include <time.h>
-unsigned int sleep(unsigned int seconds)
-{
-    struct timespec ts = (struct timespec) { .tv_sec = seconds, .tv_nsec = 0};
-	return nanosleep(&ts, NULL);
-}
